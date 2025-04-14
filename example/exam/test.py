2046841f88c5ee6accbe08f0f576bfeb95fa6855
BobasB <bbuhyl@cisco.com>
2025-04-14 22:39:36 +0300

    доповнення

--- example/exam/test.py
+++ example/exam/test.py
@@ -3,6 +3,7 @@ from main import count_string
 
 class TestFunction(unittest.TestCase):
     def test_my_function(self):
+        self.assertIsInstance(count_string("no"), int)
         self.assertEqual(count_string("Два"), 3)
 
 if __name__ == '__main__':
